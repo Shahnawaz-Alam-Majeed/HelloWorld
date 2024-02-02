bdc6fe34aca12c95cdd1cc48d3ec2b2b0140ea53
Shahnawaz-Alam <snz.alam26@gmail.com>
2024-02-02 16:32:51 +0300

    Added new Message

--- HelloWorld/src/com/scs/tut/Helloworld.java
+++ HelloWorld/src/com/scs/tut/Helloworld.java
@@ -15,6 +15,8 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Hi Java World");
+		
 	}
 
 }
